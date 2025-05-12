5a7c6d55c1649473b08f477bfde107c10eaad6dc
ADMIN <ADMIN@PC302>
2025-05-12 09:01:09 +0700

    Add project files.

---
+++ WebsiteBanHang/Models/MayTinhModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebsiteBanHang.Models
+{
+    public class MayTinhModel
+    {
+        public double So1 { get; set; }
+        public double So2 { get; set; }
+        public string PhepTinh { get; set; }
+    }
+}
